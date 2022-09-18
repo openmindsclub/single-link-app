5df03ad97d7835825c9bb0bd11dd6eeb083d3136
Lilia Mehamli <lilia.mehamli@protonmail.com>
2022-09-18 16:34:16 +0100

    change color code

--- js/main.js
+++ js/main.js
@@ -1,26 +1,36 @@
 var color = window.location.href.split("#")[1];
 var background;
 var titleColor;
+var tableColor;
+var listItemColor;
 switch (color) {
   case "r":
     // red
     background = "./images/red.png";
-    titleColor = "#e82b1f";
+    titleColor = "#ed333bff";
+    tableColor = "rgb(0, 0, 0, 0.4)";
+    listItemColor = "#fff";
     break;
   case "g":
     // green
     background = "./images/green.png";
-    titleColor = "#49ac36";
+    titleColor = "#33d17aff";
+    tableColor = "rgb(255, 255, 255, 0.4)";
+    listItemColor = "#000";
     break;
   case "y":
     // yellow
     background = "./images/yellow.png";
-    titleColor = "#f8ae28";
+    titleColor = "#f5c211ff";
+    tableColor = "rgb(255, 255, 255, 0.4)";
+    listItemColor = "#000";
     break;
   default:
     // blue
     background = "./images/blue.png";
-    titleColor = "#1c64b4";
+    titleColor = "#3584e4ff";
+    tableColor = "rgb(0, 0, 0, 0.4)";
+    listItemColor = "#fff;";
     break;
 }
 
@@ -31,3 +41,8 @@ el.style.backgroundImage = `url(${background})`;
 // change color of title
 var title = document.getElementsByClassName("title");
 title[0].style.color = titleColor;
+
+// change background color of table
+var table = document.getElementsByClassName("table");
+table[0].style.backgroundColor = tableColor;
+table[0].style.color = listItemColor;
